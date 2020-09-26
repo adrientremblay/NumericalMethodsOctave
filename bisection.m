c0696238a6b60d336b7d48865c411bbb42a04f3f
Adrien Tremblay <adrien@adrientremblay.com>
2020-09-26 16:48:37 -0400

    first commit

---
+++ bisection.m
@@ -0,0 +1,20 @@
+function ret = bisection(f, a, b, iterations)
+  for i=1:iterations
+    % calculating the bisection
+    x_i = (a+b)/2
+    
+    % applying f to all the inputs
+    f_a = f(a)
+    f_b = f(b)
+    f_xi = f(x_i)
+    
+    if (f_a*f_xi < 0)
+      b = x_i
+    else
+      a = x_i
+    endif
+   
+  endfor 
+   
+  ret = f_xi;
+endfunction
