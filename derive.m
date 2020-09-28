a2a6421c4d6cfd74afb01a3063a9249131b9f947
Adrien Tremblay <adrien@adrientremblay.com>
2020-09-28 19:55:16 -0400

    Create a derivative function

---
+++ derive.m
@@ -0,0 +1,9 @@
+function diff_f = derive(f)
+  % loading symbolic package
+  pkg load symbolic
+  syms x;
+  % converting f into a symbolic function and calculating it's derivative
+  symb_f = f(x);
+  diff_symb_f = diff(symb_f);
+  diff_f = function_handle(diff_symb_f);
+endfunction
