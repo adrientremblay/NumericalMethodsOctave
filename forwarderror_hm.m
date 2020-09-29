4a9d3afc69643081f7e48db693503861996c12f1
Adrien Tremblay <adrien@adrientremblay.com>
2020-09-28 20:11:48 -0400

    function to calculate forward error for roots of high multiplicity

---
+++ forwarderror_hm.m
@@ -0,0 +1,4 @@
+% calculates the forward error for a function with a high multiplicity
+function res = forwarderror_hm(f, fm, m, xr)
+  res = abs( (factorial(m) .* f(xr)) ./ (fm(xr)))^(1./m)
+endfunction
